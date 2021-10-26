5a23dd52850625ffdd6b599373bae7279d10d6f1
NadezhdaTarasova <tarasova_na_19@mf.grsu.by>
2021-10-26 11:02:35 +0300

    Database architecture
    added a table with detailed information about dishes(1-1)

--- liquibase/changelogs/homework-2/sql/06.create-dishes-table.sql
+++ liquibase/changelogs/homework-2/sql/06.create-dishes-table.sql
@@ -1,9 +1,7 @@
 CREATE TABLE dishes (
     id SERIAL,
-    dish_type VARCHAR(30) UNIQUE NOT NULL,
+    dish_type VARCHAR(30) NOT NULL,
     name VARCHAR(100) NOT NULL,
-    caloric_content INTEGER NOT NULL,
-    CONSTRAINT dishes_pkey PRIMARY KEY (id),
     CONSTRAINT dishes_dish_type_key UNIQUE (dish_type),
-    CONSTRAINT dishes_caloric_content_check CHECK ((caloric_content < 1000))
+    CONSTRAINT dishes_pkey PRIMARY KEY (id)
 );
